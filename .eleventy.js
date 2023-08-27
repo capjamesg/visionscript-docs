8d3bf5a530a250a50827a8cc6a9cbb0c01d311bd
James Gallagher <jamesg@jamesg.blog>
2023-08-27 19:22:19 +0100

    add hovercards

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,9 @@
+const fs = require("fs");
+const fm = require("front-matter");
+
+const docs = fs.readdirSync("docs");
+
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets", "assets");
     
@@ -8,4 +14,23 @@ module.exports = function(eleventyConfig) {
             return a.fileSlug.localeCompare(b.fileSlug);
         });
     });
+
+    // open all files in docs, get the title from their front matter
+    const hovercards = {};
+
+    for (let i = 0; i < docs.length; i++) {
+        const doc = docs[i];
+        const docContents = fs.readFileSync("docs/" + doc, "utf8");
+
+        const docFrontMatter = fm(docContents);
+
+        var url = doc.replace(".md", "");
+
+        hovercards["https://visionscript.dev/docs/" + url] = {
+            title: docFrontMatter.attributes["title"],
+            description: docFrontMatter.attributes["definition"]
+        };
+    }
+
+    fs.writeFileSync("assets/definitions.js", "var hovercards = " + JSON.stringify(hovercards) + ";");
 };
